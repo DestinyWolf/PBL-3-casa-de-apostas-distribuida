b25aea59fc444a154cad6f4ddb6e49d9d19a1d3f
Maike de Oliveira Nascimento <maikoy606@hotmail.com>
2024-12-03 18:14:04 -0300

    modified:   smartcontract/aposta.sol 	modified:   smartcontract/workToken.sol

--- smartcontract/aposta.sol
+++ smartcontract/aposta.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.0;
 
 import "./workToken.sol";
 
-contract aposta {
+contract BetContract {
 
     //enum pra definir o status da aposta
     enum Status {CLOSED, OPEN}
 
     //variaveis
-    address[] private apostadores;
-    address private winners;
+    //address[] private bettors;
+    address[] private allBettors;
     address private owner;
     address private tokenAddress;
+    uint256 private opc1;
+    uint256 private opc2;
+    uint256 private amoutOpc1;
+    uint256 private amoutOpc2;
 
     Status betStatus;
 
@@ -26,28 +30,52 @@ contract aposta {
         owner = msg.sender;
         tokenAddress = token;
         betStatus = Status.OPEN;
+        opc1 = 0;
+        opc2 = 0;
+        amoutOpc1 = 0;
+        amoutOpc2 = 0;
     }
 
-    event Bet(address _to, address _from, uint256 _amount);
-    event Winners(address wallet, uint256 amount);
+    mapping (uint => address[]) bettors;
+
+    event Bet(address _to, address _from, uint256 _amount, uint256 opc);
+    event Winners(address[] wallet, uint256 amount, uint256 result);
 
 
-    function bet(uint256 amount) public returns(bool) {
+    function bet(uint256 amount, uint256 opc) public returns(bool) {
         require(betStatus == Status.OPEN, "bet closed");
         require(amount == 500, "bet amount must be 500");
+        if (opc % 2 == 0) {
+            opc1 += 1;
+            amoutOpc1 += amount;
+            bettors[opc].push(msg.sender);
+        } else {
+            opc2 += 1;
+            amoutOpc2 += amount;
+            bettors[opc].push(msg.sender);
+        }
         WorkToken(tokenAddress).transferFrom(msg.sender, address(this), amount); //recolhe o dinheiro da pessoa
-        apostadores.push(msg.sender);
-        emit Bet(msg.sender, address(this), amount);
+        allBettors.push(msg.sender);
+        emit Bet(msg.sender, address(this), amount, opc);
         return true;
+    }
+
+    function CalcOdd() public view returns(uint256[] memory) {
+        return odds;
     }
 
     function giftWinners() public isOwner returns (bool) {
         require(betStatus == Status.OPEN, "bet closed");
-        uint number = random() % apostadores.length;
+        uint number = random() % 2;
         uint balance = WorkToken(tokenAddress).balanceOf(address(this)); //valor armazenado no contrato
-        address winner = apostadores[number];
-        WorkToken(tokenAddress).transfer(winner, balance); // envia pra pessoa o dinheiro pra quem ganhou 
-        emit Winners(winner, balance);
+        //memory address[] winner = bettors[number];
+
+        uint256 amoutForUser = balance / (number == 0 ? opc1 : opc2);
+        for(uint256 i = 0; i< bettors[number].length; i++) {
+            WorkToken(tokenAddress).transfer(bettors[number][i], amoutForUser);
+        }
+        //WorkToken(tokenAddress).transfer(winner, balance); // envia pra pessoa o dinheiro pra quem ganhou 
+        emit Winners(bettors[number], balance, number);
 
         return true;
     }
@@ -58,7 +86,7 @@ contract aposta {
     }
 
     function getPlayers() public view returns(address[] memory) {
-        return apostadores;
+        return allBettors;
     }
 
     function statusClose() public isOwner {
@@ -74,6 +102,6 @@ contract aposta {
     }
     //gera um numero aleatorio
     function random() private view returns(uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, apostadores)));
+        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, allBettors)));
     }
 }
--- smartcontract/workToken.sol
+++ smartcontract/workToken.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.0;
 
-//deckaracao de variaveis
-
 contract WorkToken {
 
-    string public name = "Lulacoins Token";
-    string public symbol = "LCT";
-    string public standard = "WorkToken v1.0";
-    uint256 public totalSupply;
+    //declaração de variaveis
+    string private _name = "Lulacoins Token";
+    string private _symbol = "LCT";
+    string private standard = "WorkToken v1.0";
+    uint256 private _totalSupply;
+    uint256 private _decimals = 18;
 
+    //eventos
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
+    // allowance e balanceOf 2 mappings
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
 
-    constructor(uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply; 
+    //construtor, função que é execultada apenas uma unica vez durante o deploy do contrato
+    constructor(uint256 _initialSupply) {
+        balanceOf[msg.sender] = _initialSupply * 10 ** _decimals;
+        _totalSupply = _initialSupply  * 10 ** _decimals; 
     }
 
+    //trasnferencia de tokens interna de uma carteira para outra
     function transfer(address _to, uint256 _value) public returns(bool) {
         require(balanceOf[msg.sender] >= _value, "no have cash strange");
         balanceOf[msg.sender] -= _value;
@@ -30,12 +34,14 @@ contract WorkToken {
         return true;
     }
 
+    //aprova que um terceiro possa realizar transferencia de uma carteira para outra
     function approve(address _spender, uint256 _value) public returns(bool success) {
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
 
+    //transferencia externa de uma carteira para outra
     function transferFrom(address _from, address _to, uint256 _value) public returns(bool success) {
         require(balanceOf[_from] >= _value, "he not have cash");
         require(allowance[_from][msg.sender] >= _value, "spender limit exceeded");
@@ -44,5 +50,27 @@ contract WorkToken {
         allowance[_from][msg.sender] -= _value;
         emit Transfer(_from, _to, _value);
         return true;
+    }
+    
+    //funções de apenas visualização, não alteram o estado do contrato
+
+    //retorna o nome dos tokens
+    function name() public view returns(string memory) {
+        return _name;
+    }
+
+    //retorna o simbolo dos tokens
+    function symbol() public view returns(string memory) {
+        return _symbol;
+    }
+
+    //retorna o valor da menor fração de um token
+    function decimals() public view returns(uint256) {
+        return _decimals;
+    }
+
+    //retorna o total de tokens existentes
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
     }
 }
